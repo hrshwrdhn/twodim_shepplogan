8beecac88b236845fd37faab72bf8de32fe23798
HARSH WARDHAN   SINGH <hrshwrdhn@gmail.com>
2019-11-20 21:59:38 +0530

    Update mainfile.m

--- mainfile.m
+++ mainfile.m
@@ -12,6 +12,6 @@ nang= 100;
 nt= 127;
 t= -1*sqrt(2): 2*sqrt(2)/(nt-1) : sqrt(2);
 ang= 0:180/(nang):179.99;
-proj= testproj(ang,t);
+proj= projection_radon2(ang,t);
 tau = 2*sqrt(2)/(nt-1);
 shepp_rec = wedgefilter_1(proj,tau,ang);
